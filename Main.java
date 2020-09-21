5c6f259c55413eb8f7a92f45f4acf5aa23e98562
CSBlues <66141547+CSBlues@users.noreply.github.com>
2020-09-21 09:04:32 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,37 @@
+/*
+Borrowed from https://repl.it/@LongNguyen18/Unit2MathClassLab#Main.java
+*/
+
+class Main {
+    public static void main(String[] args) {
+        // fill in the code
+        // create a random double in the range [0, 1), store in variable a, print out a
+
+        // create a random double in the range [0, 10), store in variable b, print out b
+
+
+        // print out a random double in the range [3,7), 
+
+        // print out a random double in the range [-2,8), 
+
+
+        // print out a random integer from the set {0, 1, 2, ..., 8}
+
+
+        // print out a random integer from the set {1, 2, ..., 10}
+
+        // print out a random integer from the set {5, 6, ..., 16}
+
+
+        // create a variable called min which is the smallest possible integer, print it 
+
+        // subtract 1 from min, print it, what happened?
+
+
+        // create a variable called max which is the largest possible integer, print it 
+
+        // add 1 to max, print it, what happened?
+
+
+    }
+}
